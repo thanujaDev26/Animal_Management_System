7b0bb16f8fb5770c47a482c159098bfd89d7bc07
Ayesha pemal <68103074+ayeshpemal@users.noreply.github.com>
2024-06-17 20:05:11 +0530

    Delete src/main directory

--- src/main/java/Generator.java
+++
@@ -1,9 +0,0 @@
-import org.mindrot.jbcrypt.BCrypt;
-
-public class Generator {
-    public static void main(String[] args) {
-        String password = "Thanuja20@";
-        String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt(12));
-        System.out.println(hashedPassword);
-    }
-}
